7902d1bce77eda70b0e7b68ce89d70bc1f12350e
Apostolia Ntikou <antasimis@gmail.com>
2023-12-21 01:35:40 +0200

    preupgrade

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,21 +1,20 @@
-const t = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), p = [
-  t + "/_app/immutable/entry/app.b0155a1b.js",
-  t + "/_app/immutable/nodes/0.c89226d3.js",
-  t + "/_app/immutable/nodes/1.73fa5ddc.js",
-  t + "/_app/immutable/assets/2.360693db.css",
-  t + "/_app/immutable/nodes/2.ee730053.js",
-  t + "/_app/immutable/chunks/index.34c7cd5a.js",
-  t + "/_app/immutable/chunks/index.e5f0f49b.js",
-  t + "/_app/immutable/chunks/singletons.afd5c1d4.js",
-  t + "/_app/immutable/entry/start.da16e608.js"
+const t = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), l = [
+  t + "/_app/immutable/entry/app.324c8d5d.js",
+  t + "/_app/immutable/nodes/0.be4f3342.js",
+  t + "/_app/immutable/nodes/1.de50b6af.js",
+  t + "/_app/immutable/assets/2.7d111529.css",
+  t + "/_app/immutable/nodes/2.0f1ad7f0.js",
+  t + "/_app/immutable/chunks/index.504075d7.js",
+  t + "/_app/immutable/chunks/index.c2b02156.js",
+  t + "/_app/immutable/chunks/singletons.71ef5117.js",
+  t + "/_app/immutable/entry/start.834397fd.js"
 ], d = [
-  t + "/.nojekyll",
   t + "/chain-icon.png",
   t + "/favicon.png",
   t + "/gear-svgrepo-com.svg",
   t + "/manifest.json",
   t + "/vars.css"
-], i = "1688656324779", c = self, r = `cache${i}`, h = p.concat(d), m = new Set(h);
+], i = "1696608148941", c = self, r = `cache${i}`, h = l.concat(d), m = new Set(h);
 c.addEventListener("install", (s) => {
   s.waitUntil(
     caches.open(r).then((e) => e.addAll(h)).then(() => {
@@ -47,8 +46,8 @@ async function u(s) {
 c.addEventListener("fetch", (s) => {
   if (s.request.method !== "GET" || s.request.headers.has("range"))
     return;
-  const e = new URL(s.request.url), a = e.protocol.startsWith("http"), n = e.hostname === self.location.hostname && e.port !== self.location.port, o = e.host === self.location.host && m.has(e.pathname), l = s.request.cache === "only-if-cached" && !o;
-  a && !n && !l && s.respondWith(
+  const e = new URL(s.request.url), a = e.protocol.startsWith("http"), n = e.hostname === self.location.hostname && e.port !== self.location.port, o = e.host === self.location.host && m.has(e.pathname), p = s.request.cache === "only-if-cached" && !o;
+  a && !n && !p && s.respondWith(
     (async () => o && await caches.match(s.request) || u(s.request))()
   );
 });
